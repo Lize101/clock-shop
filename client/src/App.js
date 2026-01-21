2b3063bc408184a51fd062d4d54b5d1454fd9a8d
Lizdigital <lizetteclaireh@gmail.com>
2026-01-21 14:38:32 +0000

    Setup React frontend with header

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>⏰ Clock Shop</h1>
+        <p>Welcome to our clock e-commerce site!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
